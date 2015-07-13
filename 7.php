9959da79cdd876d204eb9b8a222228650d65b49f
galzalait <galzalait@gmail.com>
2015-07-13 23:11:15 +0300

    Initial commit

---
+++ 7.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gal zalait | galzalait@gmail.com
+ * Date: 13/07/2015
+ * Time: 23:02
+ */
+/**
+ * Created by PhpStorm.
+ * User: Gal2
+ * Date: 13/07/2015
+ * Time: 23:02
+ *
+ */
+
+echo "hello world!";
+?>
